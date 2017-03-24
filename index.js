c263c953732e959a8d716b81e58f10995104b30a
zander <zand3rs@gmail.com>
2017-03-24 15:16:10 +0800

    added supported argument options

--- index.js
+++ index.js
@@ -8,14 +8,28 @@ var args = process.argv.slice(2);
 
 program
   .version(package.version, "-v, --version")
+  .option("--connect <connect>", "Db connection string")
+  .option("--schema <schema>", "Db schema")
   .option("--from <from>", "Source folder or file")
   .option("--to <to>", "Destination table")
-  .option("-r, --recursive [true|false]", "Recursive copy", function(v) {return v !== "false"}, false)
-  .option("--access-key [access_key]", "Access key")
-  .option("--secret-key [secret_key]", "Secret key")
+  .option("--access-key <access_key>", "AWS access key")
+  .option("--secret-key <secret_key>", "AWS secret key")
+  .option("-r, --recursive [true|false]", "Recursive copy, default=false", function(v) {return v !== "false"}, false)
+  .option("-f, --force [true|false]", "Non-interactive, default=false", function(v) {return v !== "false"}, false)
   .parse(process.argv);
 
+//-- show help for empty arguments...
 !args.length && program.help();
 
+//-- run the program...
 run(program.opts());
 
+//------------------------------------------------------------------------------
+//-- handle uncaught exceptions...
+
+//process.on("uncaughtException", function(e) {
+//  console.log("Ooops, something went wrong!!!\n", e);
+//  process.exit();
+//})
+
+//==============================================================================
