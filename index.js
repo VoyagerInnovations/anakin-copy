06ff1e332fcd6f4d1a91a4e27f26750ffab19f6c
zander <zand3rs@gmail.com>
2017-04-04 18:11:20 +0800

    added processors option for setting batch processing limit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+var _ = require("lodash");
+
 var package = require("./package.json");
 var run = require("./lib/run");
 
@@ -17,6 +19,7 @@ program
   .option("--access-key <access_key>", "AWS access key")
   .option("--secret-key <secret_key>", "AWS secret key")
   .option("--ignore-attrs <attrs_list>", "Ignore attributes")
+  .option("-p, --processors <processors>", "Max processors, default=4", function(v) {return _.toInteger(v)}, 4)
   .option("-r, --recursive [true|false]", "Recursive copy, default=false", function(v) {return v !== "false"}, false)
   .option("-f, --force [true|false]", "Non-interactive, default=false", function(v) {return v !== "false"}, false)
   .option("-d, --debug [true|false]", "Show debug messages, default=false", function(v) {return v !== "false"}, false)
